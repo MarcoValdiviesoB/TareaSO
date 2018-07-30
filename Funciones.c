82fb56b90d66b998e9787265dd78ea0fdb98e653
MarcoValdiviesoB <marco.valdiviesob@gmail.com>
2018-07-30 02:50:39 +0000

    Update Funciones.c

--- Funciones.c
+++ Funciones.c
@@ -1,1 +1,24 @@
+#include <stdio.h>qs
+#include <string.h>
 
+
+/*
+Tipos:
+	0 = Numero
+	1 = Funcion
+	2 = Variable
+*/
+
+struct simbolo{
+	char valor;
+	int tipo;
+}Simbolo;
+
+int main(){
+	
+	FILE *file = fopen("funciones.txt");
+	char l;
+	
+	fclose(file);	
+	return 0;
+}
